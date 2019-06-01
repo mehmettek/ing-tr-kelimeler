d2eaf4137a78ce03e9abe53d7787f9be034f478e
Mehmet Tek <mehmet_tek@windowslive.com>
2019-06-01 17:08:18 +0300

    first commit

---
+++ android/app/src/main/java/com/example/ingilizce_turkce_sozluk/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ingilizce_turkce_sozluk;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,50 @@
+import 'dart:convert';
+ 
+import 'package:flutter/material.dart';
+
+ class MyHomePage extends StatefulWidget {
+   @override
+   _MyHomePageState createState() => _MyHomePageState();
+ }
+ 
+ class _MyHomePageState extends State<MyHomePage> {
+   List ingkelimeler;
+   List trkelimeler;
+ 
+
+   @override
+   Widget build(BuildContext context) {
+     return Scaffold(
+       appBar: AppBar(title: Text("Sozluk"),),
+        body: SafeArea(
+          child: Container(
+          padding: EdgeInsets.all(15.0),
+              child: FutureBuilder(
+                future: DefaultAssetBundle.of(context).loadString("assets/data/kelimeler.json"),
+                builder: (context,cevap){
+                ingkelimeler=jsonDecode(cevap.data.toString().toLowerCase());
+                return ListView.separated(
+              separatorBuilder: (context, i) => Divider(
+        color: Colors.black,),
+         itemCount:ingkelimeler.length,
+        itemBuilder: (context,i){
+          return Row(
+            children: <Widget>[
+              new Text(ingkelimeler[i]['kelime'] +"  :   "+ingkelimeler[i]['anlami'] ),
+          ],
+
+          );  
+  
+        },
+                );
+
+                },
+              ),
+  ),
+
+          ),
+
+        
+        );
+   }
+ } 
